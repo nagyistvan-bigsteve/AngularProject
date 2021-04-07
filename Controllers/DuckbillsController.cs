6f01b5829135f18ff97fd7f579fe3d93e370d03d
NagyIstvan <istvannagy8000@gmailcom>
2021-04-07 23:41:58 +0300

    Add project files.

---
+++ Controllers/DuckbillsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+
+namespace Project1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DuckbillsControllers : ControllerBase
+    {
+        private readonly ILogger<DuckbillsControllers> _logger;
+
+        public DuckbillsControllers(ILogger<DuckbillsControllers> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<AngularProject.Models.Duckbill> Get()
+        { 
+            return new AngularProject.Models.Duckbill[2]
+            {
+                new AngularProject.Models.Duckbill() { ID = Guid.NewGuid(), Name = "Duckbill 1" },
+                new AngularProject.Models.Duckbill() { ID = Guid.NewGuid(), Name = "Duckbill 2" }
+            };
+        }
+    }
+}
---
+++ Models/Duckbill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AngularProject.Models
+{
+    public class Duckbill
+    {
+        [IgnoreDataMemberAttribute]
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
